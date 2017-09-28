a8b20d4a0e4f8b2a544417c6db9d83197fd3498b
MDNoorUddin <31165772+MDNoorUddin@users.noreply.github.com>
2017-09-28 15:24:39 +0600

    Positive Negative Sign

--- 1294.cpp
+++ 1294.cpp
@@ -1,29 +1,44 @@
 #include<bits/stdc++.h>
 using namespace std;
-bool isPalindrome(int n){
-    int sum=0,a=n;
-    while(n)
-    {
-        sum=sum*10+n%10;
-        n/=10;
-    }if(a==sum)
-    return true;
-    else return false;
-}
+bool isPalindrome(int n);
 int main()
 {
     int t;
     scanf("%d",&t);
-    for(int i=1;i<=t;i++)
+    int casE=1;
+    while(t--)
     {
         int a;
         scanf("%d",&a);
         if(isPalindrome(a))
-            printf("Case %d: Yes\n",i);
+            printf("Case %d: Yes\n",casE++);
         else
-            printf("Case %d: No\n",i);
+            printf("Case %d: No\n",casE++);
     }
 
     return 0;
 }
-
+bool isPalindrome(int n){
+    int sum=0;
+    int temp;
+    int a=n;
+    int i=1,digit=0;
+    while(n!=0)
+    {
+        n/=10;
+        digit++;
+    }
+    n=a;
+    for(int j=1;j<digit;j++)
+        i=i*10;
+    while(n!=0)
+    {
+        temp=n%10;
+        n/=10;
+        sum+=(temp*i);
+        i=i/10;
+    }
+    if(a==sum)
+    return true;
+    else return false;
+}
